35db79e5a3d53842fcf2dea0a4b5d76cfe794f82
user <user@users-MacBook-Air.local>
2020-08-26 20:10:44 +0700

    make it better

--- app.js
+++
@@ -1,6 +0,0 @@
-import app from './backend/server';
-import config from './backend/config';
-
-const { PORT } = config;
-
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
--- backend/server.js
+++ backend/server.js
@@ -38,13 +38,13 @@ app.get("/api/products/:id", (req, res) => {
     }
 });
 
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+app.use(express.static(path.join(__dirname, '/../frontend')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../frontend/index.html'));
+  });
+
+app.listen(config.PORT, () => {
+  console.log('serve at http://localhost:5000');
+});
 
 export default app;
